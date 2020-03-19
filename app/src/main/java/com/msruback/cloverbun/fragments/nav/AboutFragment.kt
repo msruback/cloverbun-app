161bee34c5ff4d46aaeea62513e6c8cf9d6ef80c
Matthew Rubacky <mattrubacky@gmail.com>
2020-03-19 11:18:24 -0400

    Moved fragments to their own folder

--- app/src/main/java/com/msruback/cloverbun/fragments/nav/AboutFragment.kt
+++ app/src/main/java/com/msruback/cloverbun/fragments/nav/AboutFragment.kt
@@ -1,13 +1,14 @@
-package com.msruback.cloverbun
+package com.msruback.cloverbun.fragments.nav
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.msruback.cloverbun.R
 
 
-class StatFragment : Fragment() {
+class AboutFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,12 +19,11 @@ class StatFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_stat, container, false)
+        return inflater.inflate(R.layout.fragment_about, container, false)
     }
 
     companion object {
-        @JvmStatic
-        fun newInstance() =
-            StatFragment()
+
+        fun newInstance() = AboutFragment()
     }
 }
--- app/src/main/java/com/msruback/cloverbun/fragments/nav/StatFragment.kt
+++ app/src/main/java/com/msruback/cloverbun/fragments/nav/StatFragment.kt
@@ -1,13 +1,14 @@
-package com.msruback.cloverbun
+package com.msruback.cloverbun.fragments.nav
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.msruback.cloverbun.R
 
 
-class AboutFragment : Fragment() {
+class StatFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,11 +19,12 @@ class AboutFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false)
+        return inflater.inflate(R.layout.fragment_stat, container, false)
     }
 
     companion object {
-
-        fun newInstance() = AboutFragment()
+        @JvmStatic
+        fun newInstance() =
+            StatFragment()
     }
 }
